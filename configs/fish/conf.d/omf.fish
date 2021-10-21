ed6c640053de45e1085441211aad0bbd8b29c6b8
fy-st0rm <slokpradhan2062@gmail.com>
2021-10-21 19:48:33 +0545

    New Fish prompt

--- configs/fish/conf.d/omf.fish
+++ configs/fish/conf.d/omf.fish
@@ -1,4 +1,6 @@
 
-alias ls='exa -la'
+alias ls='exa'
+alias ll='exa -la'
+alias la='exa -a'
 alias neofetch='neofetch --ascii_colors 4 --colors 0 0 0 4 0 7'
 
--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -5,8 +5,57 @@ end
 # To remove greeting
 set fish_greeting
 
+# Git status
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'no'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+
+# Git colors
+set __fish_git_prompt_color cyan
+set __fish_git_prompt_color_branch brmagenta
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_color_dirtystate red
+set __fish_git_prompt_color_untrackedfiles red
+set __fish_git_prompt_color_stagedstate bryellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate 'X'
+set __fish_git_prompt_char_stagedstate '+'
+set __fish_git_prompt_char_untrackedfiles '!'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑ '
+set __fish_git_prompt_char_upstream_behind '↓ '
+set __fish_git_prompt_char_upstream_equal ''
+
+
+function fish_prompt
+	set_color brred
+	printf '['
+	
+	set_color bryellow
+	printf '%s@' "$USER"
+
+	set_color brblue 
+	printf '%s ' "$hostname"
+
+	set_color brgreen
+	printf '%s' (prompt_pwd)
+	
+	set_color brred
+	printf ']'
+	
+	set_color normal
+	printf '%s' (fish_git_prompt)
+	set_color brcyan
+	printf '$ '
+end
+
+
 # Enabling starship prompt
-starship init fish | source
+# starship init fish | source
 
-sfetch
-
+# [Custom] Auto start
+# sfetch
+colorscript -e panes
