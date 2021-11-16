deebe1e83d4b4ce8b7db24b47e1739b9839cc86b
fy-st0rm <slokpradhan2062@gmail.com>
2021-11-16 22:06:12 +0545

    Color tuiking in sfetch

--- scripts/python/sfetch.py
+++ scripts/python/sfetch.py
@@ -5,12 +5,12 @@ import random
 
 # Colors for terminal
 class colors:
-    PURPLE = '\033[95m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
+    PURPLE = '\033[35m'
+    BLUE = '\033[34m'
+    CYAN = '\033[36m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    RED = '\033[31m'
     DEFAULT = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
