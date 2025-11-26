169bd83d415762012fd3d82f55093802473eef6e
fy-st0rm <slokpradhan2062@gmail.com>
2025-11-26 15:47:50 +0545

    #Updates: + Solarized theme added to alacritty + i3 + New polybar

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -8,9 +8,10 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar systray 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar left 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar center 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar right 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar systray 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar left 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar center 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar right 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
